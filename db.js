bb70b2ae54e22ebcf3095c40d9d90b8aab6c9968
RalphsCode <RalphsCode@gmail.com>
2025-01-21 16:36:05 -0800

    slight change to the db conn file. ENV variables not exposed.

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Load environment variables from .env file
 
 let DB_URI;
 
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "development") {
   DB_URI = process.env.TEST_DB_URI;
 } else {
   DB_URI = process.env.DATABASE_URL;
